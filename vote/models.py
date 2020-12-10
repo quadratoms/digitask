c41442bbd16227717b6a255573259f981920c22a
quadratoms <quadratoms30@gmail.com>
2020-12-10 12:46:56 +0100

    dgitask

--- vote/models.py
+++ vote/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Voter(models.Model):
-    user= models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_name= models.CharField(max_length=20)
-    last_name= models.CharField(max_length=20)
-    email= models.EmailField(max_length=100)
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name= models.CharField(max_length=20, null=True)
+    last_name= models.CharField(max_length=20, null=True)
     birthday = models.IntegerField(default=18)
-    secure_pin=models.CharField(max_length=7)
-    
-    
+    email= models.EmailField(max_length=100)
+    secure_pin=models.CharField(max_length=7, null=True)
+
+@receiver(post_save, sender=User)
+def creat_user_voter(sender, instance, created, **kwargs):
+    if created:
+        Voter.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_use_rvoter(sender,instance, **kwargs):
+    instance.voter.save()
+
+
+
     
     
     
--- vote/views.py
+++ vote/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import Entryform
 from .models import Voter, Votes
 from django.contrib.auth.models import auth, User
+from django.contrib.auth.decorators import login_required
+
 
 
 def home(request):
@@ -38,49 +40,54 @@ def entry(request):
         else:
 
             user = User.objects.create_user(
-                username=username, first_name=first_name, last_name=last_name, password=password, age=age, email=email)
-            user.save()
+                username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+            b = user.save()
+            a = User.objects.get(username=username)
+            a = Voter.objects.get(user=a)
+            a.birthday = age
+            a.save()
             user = auth.authenticate(
                 request, username=username, password=password)
             auth.login(request, user)
-            form.user_id = request.user
 
             return redirect('vote')
 
     return render(request, 'entry.html', {'form': form})
 
-
+@login_required
 def vote(request):
     user = request.user
     print(user)
-    #profile = Voter.objects.get(user=user)
+    # profile = Voter.objects.get(user=user)
     allow = True
-    choice = None
+
     a = Votes.objects.filter(user=user)
     print(len(a))
-    l=[len(Votes.objects.filter(vote='Donald Trump')), len(Votes.objects.filter(vote='Joe Biden'))]
+    l = [len(Votes.objects.filter(vote='Donald Trump')),
+             len(Votes.objects.filter(vote='Joe Biden'))]
     if len(a) == 0:
-
+        choice = None
         if 'biden' in request.POST:
             v = Votes()
             v.user = user
             v.vote = 'Joe Biden'
             v.save()
-            return redirect('entry/')
+            return redirect('vote')
 
         if 'trump' in request.POST:
             v = Votes()
             v.user = user
             v.vote = 'Donald Trump'
             v.save()
-            return redirect('entry')
+            return redirect('vote')
 
     else:
         allow = False
         choice = Votes.objects.get(user=user)
+        print(choice.vote)
         
 
-    return render(request,  'vote.html', {'choice': choice, 'l':l})
+    return render(request,  'vote.html', {'choice': choice, 'l':l, 'allow':allow})
 
 
 # Create your views here.
